ed67cdd086876a235419d2bd83c7fee7a91d31ed
Mohokee <j.inger.ly.co@gmail.com>
2021-07-17 19:24:22 -0700

    Added change scene functionality, pre vaiables to change all scenes

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -11,7 +11,6 @@
     import javafx.scene.control.Button;
     import javafx.stage.Stage;
 
-    import javax.swing.plaf.basic.BasicOptionPaneUI;
     import java.io.IOException;
     import java.net.URL;
     import java.util.ResourceBundle;
@@ -19,7 +18,14 @@
 
     public class Controller implements Initializable {
         @FXML private Button exit;
+
+        //Parts Buttons
         @FXML private Button addPartsButton;
+        @FXML private Button modifyPartsButton;
+
+        //Product Buttons
+        @FXML private Button addProductButton;
+        @FXML private Button modifyProductButton;
 
         /*exitButtonPushed- Closes Program, changes: started with (ActionEvent) event as argument, and kept getting an
         error, changed to (Event event) and it now works*/
@@ -28,7 +34,7 @@
             System.exit(0);
         }
         /*Change to Scene Controls*/
-        public void addProductButtonPushed(Event event) throws IOException {
+        public void buttonPushed(Event event) throws IOException {
             //Define page button view
             var buttonName = "../view/AddProduct.fxml";
 
@@ -36,9 +42,12 @@
             Node node = (Node) event.getTarget();
             if(node.equals(addPartsButton))
                 buttonName = "../view/AddPart.fxml";
-
-
-
+            else if(node.equals(modifyPartsButton))
+                buttonName = "../view/ModifyPart.fxml";
+            else if(node.equals(addProductButton))
+                buttonName = "../view/AddProduct.fxml";
+            else if(node.equals(modifyProductButton))
+                buttonName = "../view/ModifyProduct.fxml";
             Parent addProductParent = FXMLLoader.load(getClass().getResource(buttonName));
             Scene addProductScene = new Scene(addProductParent);
 
